553ccf6ad4b63f458685659eec98a04fffd6f021
LincolnMA <machado.lincoln@gmail.com>
2019-11-01 09:50:42 -0300

    chuva colorida

--- main.py
+++ main.py
@@ -2,19 +2,29 @@ from tkinter import *
 import random
 ##Functions
 def verify_collision():
+    global enemies
+    index_colision = -1
     for i in range(0,len(enemies)):
         if enemies[i][0] <= position.x <= enemies[i][0]+enemy_size.get():
             if enemies[i][1] <= position.y <= enemies[i][1]+enemy_size.get():
-                print("Colisão!")
+                #print("Colisão!")
+                drops.set(int(drops.get()) + 1)
+                index_colision = i
+    if(index_colision > 0): del(enemies[index_colision])
 def render_enemies():
     global enemies
     screen.delete("all")
     for i in range(0,len(enemies)):
+        r = hex(random.randint(16,255))
+        g = hex(random.randint(16,255))
+        b = hex(random.randint(16,255))
+        color = "#" + r[2:] + g[2:] + b[2:]
         enemies[i][1] = enemies[i][1] + 1
         screen.create_rectangle(enemies[i][0],
                                 enemies[i][1],
                                 enemies[i][0]+enemy_size.get(),
-                                enemies[i][1]+enemy_size.get())
+                                enemies[i][1]+enemy_size.get(),
+                                fill = color)
 def generate_enemy():
     global enemies
     if run == True:
@@ -48,25 +58,37 @@ delay = IntVar()
 n = IntVar()
 delay_enemy = IntVar()
 enemy_size = IntVar()
+drops = StringVar()
+drops.set("0")
 ##SideBar
 sidebar = Frame(window)
 sidebar.grid(row = 0, column = 0)
+#Title
+lb1 = Label(sidebar,text = "RainDrops",font = ('Times', '24', 'bold italic'))
+lb1.grid(row = 0,column = 0)
 #Scale to refresh in ms
 scL1 = Scale(sidebar,orient = HORIZONTAL,from_ = 1000,to = 1,
              length = 150,label = "Refresh",variable = delay)
-scL1.grid(row = 0,column = 0)
+scL1.grid(row = 1,column = 0)
 #Scale to number of enemies
 scL2 = Scale(sidebar,orient = HORIZONTAL,from_ = 50,to = 1,
              length = 150,label = "Number of Enemies",variable = n)
-scL2.grid(row = 1,column = 0)
-#scale to delay of enemies
+scL2.grid(row = 2,column = 0)
+#Scale to delay of enemies
 scL3 = Scale(sidebar,orient = HORIZONTAL,from_ = 1000,to = 1,
              length = 150,label = "Delay of Enemies",variable = delay_enemy)
-scL3.grid(row = 2,column = 0)
-#scale to size of enemies
+scL3.grid(row = 3,column = 0)
+#Scale to size of enemies
 scL4 = Scale(sidebar,orient = HORIZONTAL,from_ = 50,to = 1,
              length = 150,label = "Size of Enemies",variable = enemy_size)
-scL4.grid(row = 3,column = 0)
+scL4.grid(row = 4,column = 0)
+#Label of Counter drops
+lb2 = Label(sidebar,text = "Score",font = ('Times', '15', 'bold italic'))
+lb2.grid(row = 5,column = 0)
+#Counter of drops
+ent = Entry(sidebar,textvariable = drops)
+ent.grid(row = 6,column = 0)
+
 ##Tela
 screen = Canvas(window,bg = "white",cursor = "man",width = 300,height = 300)
 screen.grid(row = 0,column = 1)
